6d2769ffe0a9c91211478269488d57bc15fb008b
Mohan Dogra <mohanqwerty5@gmail.com>
2022-10-01 23:16:17 -0500

    finished work

--- ms_aprioi.py
+++ ms_aprioi.py
@@ -1,11 +1,8 @@
-# -*- coding: utf-8 -*-
-
-
+# READ INPUTS 
 import csv
 import itertools
 from collections import defaultdict, OrderedDict
 
-# custom 
 def read_input(inputf,paramf):
     "read inputfile"
     data = []
@@ -18,12 +15,13 @@ def read_input(inputf,paramf):
       for row in csv_records:
         new_list = [int(item) for item in row]
         data.append(new_list)
+    # print("reading set")
         
     # param file
     uniq_vals = set(itertools.chain.from_iterable(data))
     file_iter = open(paramf, 'r')
     for line in file_iter:
-        line = line.replace(" ", "")
+        line = line.replace(" ", "").replace("{","").replace("}","")
         line = line.strip()
         if 'MIS' in line:
             key = line[line.find("(") + 1:line.find(")")]
@@ -49,196 +47,157 @@ def read_input(inputf,paramf):
         del param['rest']
 
     param = {int(k):float(v) for k,v in param.items()}
-    param = OrderedDict(sorted(param.items()))
+    # param = OrderedDict(sorted(param.items()))
 
-    # print(data,param,sdc)
-
+    # print(data,param,sdc
     return data, param, sdc
 
-# to-be modified
-def l2_cgen(FF, param, sdc, it_count, max_mis, transSize):
-    "generate cadidates for the 2-itemset"
-    s = set(frozenset([w]) for w in FF)
-    s2 = set([i.union(j) for i in s for j in s if len(i.union(j)) == 2])
-    cand = []
-    for e in s2:
-        minSup = 1
-        maxSup = 0
+def init_pass(item_list,item_count,file_data,parameters):
+  L = []
+  F = []
+  for i in range(len(item_list)):
+    if i != 0:
+        if ((item_count.get(item_list[i]) / len(file_data)) >= parameters.get(item_list[0])):
+            L.append(item_list[i])
+    else:
+        L.append(item_list[0])
+  return L
 
-        for r in e:
-            if it_count[r]/float(transSize) < minSup:
-                minSup = it_count[r]/float(transSize)
-            if it_count[r]/float(transSize) > maxSup:
-                maxSup = it_count[r]/float(transSize)
+def level2_gen(L, cand_list, sdc, item_count, inp_data, parameters):
+    for item_in_L in range(0, len(L)):
+        if (item_count[L[item_in_L]] / len(inp_data)) >= parameters[L[item_in_L]]:
+            for data in range(item_in_L + 1, len(L)):
+                if (abs((item_count[L[data]] / len(inp_data)) - (item_count[L[item_in_L]] / len(inp_data))) <= sdc) and (item_count[L[data]] / len(inp_data)) >= parameters[L[item_in_L]]:
+                    cand_list[2].append([])
+                    cand_list[2][len(cand_list[2]) - 1].append(L[item_in_L])
+                    cand_list[2][len(cand_list[2]) - 1].append(L[data])
+    cand_list[2].sort(key=lambda row: row[1])
+    return(cand_list)
 
-        if maxSup-minSup <= sdc:
-            cand.append(e)
+def level_n_gen(num,sotred_items,freq_items,cand_list,inp_data,sdc,item_count ):
+    element = 0
+    freq_level = num - 1
+    for i in range(0, len(freq_items[freq_level])):
+        for j in range(0, len(freq_items[freq_level])):
+            while (element < freq_level - 1) and (freq_items[freq_level][i][element] == freq_items[freq_level][j][element]):
+                element += 1
 
-    ## get the lowest MIS (done)
-    min_mis = dict()
+            if element == freq_level - 1:
+                if ((sotred_items[freq_items[freq_level][i][element]] < sotred_items[freq_items[freq_level][j][element]]) and (
+                        abs((item_count[freq_items[freq_level][i][element]] / len(inp_data)) - (item_count[freq_items[freq_level][j][element]] / len(inp_data))) <= sdc)):
+                    cand_list[freq_level + 1].append(list(freq_items[freq_level][i]))
+                    cand_list[freq_level + 1][len(cand_list[freq_level + 1]) - 1].append(freq_items[freq_level][j][element])
 
-    for i in cand:
-        min = 1
-        max = 0
-        maxMISItem = 0
-        for j in i:
-            if param[j] < min:
-                min = param[j]
-            if param[j] > max:
-                max = param[j]
-                maxMISItem = it_count[j]
-        min_mis[i] = min
-        max_mis[i] = maxMISItem
+            element = 0
 
-        # print(cand)
-        # print(min_mis)
-
-    return cand, min_mis
-
-# to-be modified
-def lk_cgen(L, param, sdc, k, it_count, max_mis):
-    s2 = set([i.union(j) for i in L for j in L if len(i.union(j)) == k])
-    cand = []
-    for e in s2:
-        minSup = 1
-        maxSup = 0
-        for r in e:
-            if param[r] < minSup:
-                minSup = param[r]
-            if param[r] > maxSup:
-                maxSup = param[r]
-        if maxSup-minSup <= sdc:
-            cand.append(e)
-
-    ## get the lowest MIS
-    min_mis = dict()
-    for i in cand:
-        min = 1
-        max = 0
-        max_mis_item = 0
-        for j in i:
-            if param[j] < min:
-                min = param[j]
-            if param[j] > max:
-                max = param[j]
-                max_mis_item = it_count[j]
-        min_mis[i] = min
-        max_mis[i] = max_mis_item
-
-        # print(cand)
-        # print(min_mis)
-
-    return cand, min_mis
-            
-    
-def MSA (fileData, parameters, sdc):
+def MSA (file_data, parameters, sdc):
     "MSApriori algorithm"
-    trans = []
-    for tr in fileData:
-        trans.append(set(tr))
+    item_count ={}
+    item_list = []
+    freq_items = list([] for _ in range(10))
 
-    sorted_items = [(k, v) for k, v in sorted(parameters.items(), key=lambda item: item[1])]
-    print(sorted_items)
-    transSize = len(trans)
-
-    ## construct the M
-    M =[]
-    for i in (sorted_items):
-        M.append(i[0])
-
-    # print("M",M)
-    ## get the count
+    # item_count
+    unique_items = sorted(parameters, key=parameters.__getitem__)
     
-    count=defaultdict(int)
-    for item in M:
-        for transaction in trans:
-            m={item}
-            if m.issubset(transaction):
-                count[item] += 1
-
-    # print(count)
-    # Initial pass
+    for item in unique_items:
+        item_list.append(int(item))
+        item_count[int(item)] = 0
+    # print("Item_List",item_list)
     
-    L=[]
-    start = 0
-    pivot = 0
-    for i in M:
-        # print(i,count[i],parameters[i],pivot)
-        if count[i]/float(len(trans)) >= parameters[i] and start == 0:
-            L.append(i)
-            start = 1
-            pivot = parameters[i]
-        elif count[i]/float(len(trans))  >= pivot and start == 1:
-            L.append(i)
+    for i in file_data:
+      for j in i:
+          if j not in item_count.keys():
+              item_count[j] = 0
+          else:
+              item_count[j] = item_count[j] + 1
+    # print("Item_Count",item_count)
 
-    # print(L)
+    # init-pass
+    L = init_pass(item_list,item_count,file_data,parameters)
 
-    # select the 1-frequentset
-    F = dict()
-    F_local = []
-    for i in L:
-        if count[i] / float(len(trans))  >= parameters[i]:
-            F_local.append({i})
-    F[1] = F_local
+    sotred_items = {} 
+    for i in range(len(L)):
+        sotred_items[L[i]] = i
+
+    # print(sotred_items)
+
+    # Adding 1-Itemsets to the freq_items
+    if freq_items[1] is not None:
+        for i in range(len(L)):
+            if (item_count.get(L[i]) / len(file_data)) >= parameters.get(L[i]):
+                freq_items[1].append([L[i]])
+        # print("freq_items after", freq_items)
 
 
-    # for the rest of set-size
-    k = 2
-    max_mis = dict()
-    while(len(F[k-1]) != 0):
-        print("Working on",k,"itemset ......")
-        if(k==2):
-            cd, min_mis = l2_cgen(L, parameters, sdc, count, max_mis, transSize)
+
+    level = 2
+    cand_dict = {}
+    cand_list = list([] for _ in range(10))
+    
+    while 1:
+        if not freq_items[level - 1]:
+            break
+
+        if level == 2:
+            Candidate_L = level2_gen(L, cand_list, sdc, item_count, data, parameters)
         else:
-            cd, min_mis = lk_cgen(F[k - 1], parameters, sdc, k, count, max_mis)
+            Candidate_L = level_n_gen(level,sotred_items,freq_items,cand_list,data,sdc,item_count)
 
-        ## Get the count
-        for item in cd:
-            for transaction in trans:
-                if item.issubset(transaction):
-                    count[item] += 1
-    
-        ## Select the items fro the k-frequent itemset
-        F_local = []
-        for item in cd:
-            if count[item] / float(len(trans))  >= min_mis[item]:
-                F_local.append(item)
-        F[k] = F_local
-        k+=1
+        for transac in data:
+            for cand in cand_list[level]:
+                if set(cand).issubset(set(transac)):
+                    if cand_dict.get(tuple(cand)) != None:
+                        cand_dict[tuple(cand)] += 1
+                    else:
+                        cand_dict[tuple(cand)] = 1
 
-    del(F[k-1])
+        for cand in cand_list[level]:
+            if cand_dict.get(tuple(cand)) != None:
+                if cand_dict.get(tuple(cand)) / len(data) >= parameters[cand[0]]:
+                    freq_items[level].append(cand[:])
+        level += 1
+    # print(cand_dict)
+    # print(cand_list)
+    print("freq_items:",freq_items)
+    freq_items = [x for x in freq_items if x != []]
+    return freq_items
 
-    return F
 
-def save_file(outputFile, F):
-    "Save output in the same format as requested"
-    with open(outputFile, 'w') as f:
-        k = []
-        for key in F:
-            k.append(key)
-        k = sorted(k)
-        for i in k:
-            f.write("(Length-%d %d\n\n" % (i,len(F[i])))
-            
-            for j in F[i]:
-                print("F",len(j))
-                if i == 1:
-                    tmp = next(iter(j))
-                    f.write("\t(%s) \n" % ( tuple(list(j))))
-                else:
-                    s = []
-                    f.write("")
-                    st = ""
-                    for i in set(j):
-                        st+=str(i)
-                        st+=" "
-                    
-                    f.write("\t("+st[:-1]+")\n")
-            f.write(")\n")
 
-if __name__ == "__main__":
 
-    fileData, parameters, sdc = read_input("input-data.txt","parameter-file.txt")
-    F= MSA(fileData, parameters, sdc)
-    save_file("outFile.txt", F)
-    print("\n DONE \n")
+data, parameters, sdc = read_input("data-2.txt","para-2.txt")
+F= MSA(data, parameters, sdc)
+
+result_path = "results_1.txt"
+output_file = open(result_path, "w")
+
+count = 0
+for _ in F[0]:
+    count += 1
+
+
+output_file.write("(Length-1 " + str(count) + "\n")
+if F != []:
+    for t in F[0]:
+        count += 1
+
+        output_file.write("\t" + "(" + str(t[0]) + ")\n")
+        # print("count2:",str(t[0]))
+
+output_file.write(")\n")
+
+for i in range(1, len(F)):
+    total_count = 0
+    for _ in F[i]:
+        total_count += 1
+    output_file.write("\n(Length-"+ str(i + 1) + " "+str(total_count)+"\n")
+    count = 0
+    for t in F[i]:
+        total_cnt = 0
+        for j in range(len(data)):
+            if set(data[j]) >= set(t):
+                total_cnt = total_cnt + 1
+        count += 1
+        output_file.write("\t" + "(" + str(t).replace("[", "").replace("]", "").replace(",", "") + ")\n")
+    output_file.write(")\n")
+
